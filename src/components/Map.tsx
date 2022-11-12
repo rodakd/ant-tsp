47a616a01ac5e31559f7acf7ae8de0046734cb87
Dawid Rodak <dawidrodak@yahoo.com>
2022-11-12 11:47:13 +0100

    Bump packages

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,11 @@
 import * as t from '~/types';
 
-import MapGL, { MapLayerMouseEvent, MapRef } from 'react-map-gl';
+import MapGL, { MapLayerMouseEvent, MapRef, ViewStateChangeEvent } from 'react-map-gl';
 import DeckGL from '@deck.gl/react/typed';
 import { ScatterplotLayer, PathLayer } from '@deck.gl/layers/typed';
 import { MAPBOX_TOKEN } from '~/constants';
 import { useStore } from '~/store';
-import { MutableRefObject } from 'react';
+import { MutableRefObject, useMemo } from 'react';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
@@ -32,48 +32,51 @@ export const Map = ({ mapRef }: Props) => {
 
   const drawPath = !performanceMode || status !== 'running';
 
-  console.log(viewState);
+  function handleSetViewState(e: ViewStateChangeEvent) {
+    setViewState(e.viewState);
+  }
+
+  const layers = useMemo(() => {
+    return [
+      new ScatterplotLayer({
+        id: 'scatterplot',
+        data: markers.map((m) => ({ position: m })),
+        radiusMinPixels: 7,
+        radiusMaxPixels: 7,
+        getFillColor: [0, 173, 255],
+      }),
+      drawPath &&
+        new PathLayer({
+          id: 'path-layer',
+          data: [
+            {
+              name: 'Current',
+              color: [180, 180, 180, 180],
+              path: currentTour,
+            },
+            {
+              name: 'Best',
+              color: [24, 178, 188],
+              path: bestTour,
+            },
+          ],
+          getColor: (d) => d.color,
+          widthMinPixels: 3,
+          widthMaxPixels: 9,
+        }),
+    ];
+  }, [markers, drawPath, currentTour, bestTour]);
 
   return (
     <MapGL
       {...viewState}
       ref={mapRef}
-      onMove={(e) => setViewState(e.viewState)}
+      onMove={handleSetViewState}
       mapboxAccessToken={MAPBOX_TOKEN}
       mapStyle='mapbox://styles/mapbox/light-v10'
       onClick={markerModeOn ? addMarker : undefined}
     >
-      <DeckGL
-        viewState={viewState}
-        layers={[
-          new ScatterplotLayer({
-            id: 'scatterplot',
-            data: markers.map((m) => ({ position: m })),
-            radiusMinPixels: 7,
-            radiusMaxPixels: 7,
-            getFillColor: [0, 173, 255],
-          }),
-          drawPath &&
-            new PathLayer({
-              id: 'path-layer',
-              data: [
-                {
-                  name: 'Current',
-                  color: [180, 180, 180, 180],
-                  path: currentTour,
-                },
-                {
-                  name: 'Best',
-                  color: [24, 178, 188],
-                  path: bestTour,
-                },
-              ],
-              getColor: (d) => d.color,
-              widthMinPixels: 3,
-              widthMaxPixels: 9,
-            }),
-        ]}
-      />
+      <DeckGL viewState={viewState} layers={layers} />
     </MapGL>
   );
 };
