269b14bb2f0115257f7edc2413379c971e7102d7
Dawid Rodak <dawidrodak@yahoo.com>
2023-02-10 07:57:05 +0100

    Refactor algorithms test

--- tests/algorithms.test.ts
+++ tests/algorithms.test.ts
@@ -23,198 +23,172 @@ const handleError = (err: any) => {
 
 const jestConsole = console;
 
-describe('Poland, size 291', () => {
+describe('Algorithms', () => {
   let wi: WorkerInstance;
-  let markers = prepareMarkers(Poland);
 
   beforeEach(() => {
     global.console = require('console');
     wi = new WorkerInstance();
     wi.performanceMode = true;
-    wi.markers = [...markers];
   });
 
   afterEach(() => {
     global.console = jestConsole;
   });
 
-  it('2-opt First', async () => {
-    console.log('\nRunning tests for Poland (size 291)...\n');
-    await TSP2optFirst(wi).catch(handleError);
-    console.log(`2-opt First: ${Math.floor(wi.cost)} km`);
+  describe('Poland, size 291', () => {
+    beforeEach(() => {
+      wi.markers = prepareMarkers(Poland);
+    });
+
+    it('2-opt First', async () => {
+      console.log('\nRunning tests for Poland (size 291)...\n');
+      await TSP2optFirst(wi).catch(handleError);
+      console.log(`2-opt First: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('2-opt Best', async () => {
+      await TSP2optBest(wi).catch(handleError);
+      console.log(`2-opt Best: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('Ant System', async () => {
+      await TSPAntSystem(wi).catch(handleError);
+      console.log(`Ant System: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('Lin-Kernighan', async () => {
+      await TSPLinKernighan(wi).catch(handleError);
+      console.log(`Lin-Kernighan: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('Nearest Neighbor', async () => {
+      await TSPNearestNeighbor(wi).catch(handleError);
+      console.log(`Nearest Neighbor: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('Custom', async () => {
+      const code = fs.readFileSync(require.resolve('../src/custom/nearestNeighbor'), 'utf8');
+      wi.params = { code };
+      await TSPCustom(wi).catch(handleError);
+      console.log(`Custom: ${Math.floor(wi.cost)} km`);
+    });
   });
 
-  it('2-opt Best', async () => {
-    await TSP2optBest(wi).catch(handleError);
-    console.log(`2-opt Best: ${Math.floor(wi.cost)} km`);
+  describe('Canada, size 625', () => {
+    beforeEach(() => {
+      wi.markers = prepareMarkers(Canada);
+    });
+
+    it('2-opt First', async () => {
+      console.log('\nRunning tests for Canada (size 625)...\n');
+      await TSP2optFirst(wi).catch(handleError);
+      console.log(`2-opt First: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('2-opt Best', async () => {
+      await TSP2optBest(wi).catch(handleError);
+      console.log(`2-opt Best: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('Ant System', async () => {
+      await TSPAntSystem(wi).catch(handleError);
+      console.log(`Ant System: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('Lin-Kernighan', async () => {
+      await TSPLinKernighan(wi).catch(handleError);
+      console.log(`Lin-Kernighan: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('Nearest Neighbor', async () => {
+      await TSPNearestNeighbor(wi).catch(handleError);
+      console.log(`Nearest Neighbor: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('Custom', async () => {
+      const code = fs.readFileSync(require.resolve('../src/custom/nearestNeighbor'), 'utf8');
+      wi.params = { code };
+      await TSPCustom(wi).catch(handleError);
+      console.log(`Custom: ${Math.floor(wi.cost)} km`);
+    });
   });
 
-  it('Ant System', async () => {
-    await TSPAntSystem(wi).catch(handleError);
-    console.log(`Ant System: ${Math.floor(wi.cost)} km`);
+  describe('Spain, size 1135', () => {
+    beforeEach(() => {
+      wi.markers = prepareMarkers(Spain);
+    });
+
+    it('2-opt First', async () => {
+      console.log('\nRunning tests for Spain (size 1135)...\n');
+      await TSP2optFirst(wi).catch(handleError);
+      console.log(`2-opt First: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('2-opt Best', async () => {
+      await TSP2optBest(wi).catch(handleError);
+      console.log(`2-opt Best: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('Ant System', async () => {
+      await TSPAntSystem(wi).catch(handleError);
+      console.log(`Ant System: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('Lin-Kernighan', async () => {
+      await TSPLinKernighan(wi).catch(handleError);
+      console.log(`Lin-Kernighan: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('Nearest Neighbor', async () => {
+      await TSPNearestNeighbor(wi).catch(handleError);
+      console.log(`Nearest Neighbor: ${Math.floor(wi.cost)} km`);
+    });
+
+    it('Custom', async () => {
+      const code = fs.readFileSync(require.resolve('../src/custom/nearestNeighbor'), 'utf8');
+      wi.params = { code };
+      await TSPCustom(wi).catch(handleError);
+      console.log(`Custom: ${Math.floor(wi.cost)} km`);
+    });
   });
 
-  it('Lin-Kernighan', async () => {
-    await TSPLinKernighan(wi).catch(handleError);
-    console.log(`Lin-Kernighan: ${Math.floor(wi.cost)} km`);
-  });
+  describe('France, size 2019', () => {
+    beforeEach(() => {
+      wi.markers = prepareMarkers(France);
+    });
 
-  it('Nearest Neighbor', async () => {
-    await TSPNearestNeighbor(wi).catch(handleError);
-    console.log(`Nearest Neighbor: ${Math.floor(wi.cost)} km`);
-  });
+    it('2-opt First', async () => {
+      console.log('\nRunning tests for France (size 2019)...\n');
+      await TSP2optFirst(wi).catch(handleError);
+      console.log(`2-opt First: ${Math.floor(wi.cost)} km`);
+    });
 
-  it('Custom', async () => {
-    const code = fs.readFileSync(require.resolve('../src/custom/nearestNeighbor'), 'utf8');
-    wi.params = { code };
-    await TSPCustom(wi).catch(handleError);
-    console.log(`Custom: ${Math.floor(wi.cost)} km`);
-  });
-});
+    it('2-opt Best', async () => {
+      await TSP2optBest(wi).catch(handleError);
+      console.log(`2-opt Best: ${Math.floor(wi.cost)} km`);
+    });
 
-describe('Canada, size 625', () => {
-  let wi: WorkerInstance;
-  let markers = prepareMarkers(Canada);
+    it('Ant System', async () => {
+      await TSPAntSystem(wi).catch(handleError);
+      console.log(`Ant System: ${Math.floor(wi.cost)} km`);
+    });
 
-  beforeEach(() => {
-    global.console = require('console');
-    wi = new WorkerInstance();
-    wi.performanceMode = true;
-    wi.markers = [...markers];
-  });
+    it('Lin-Kernighan', async () => {
+      await TSPLinKernighan(wi).catch(handleError);
+      console.log(`Lin-Kernighan: ${Math.floor(wi.cost)} km`);
+    });
 
-  afterEach(() => {
-    global.console = jestConsole;
-  });
+    it('Nearest Neighbor', async () => {
+      await TSPNearestNeighbor(wi).catch(handleError);
+      console.log(`Nearest Neighbor: ${Math.floor(wi.cost)} km`);
+    });
 
-  it('2-opt First', async () => {
-    console.log('\nRunning tests for Canada (size 625)...\n');
-    await TSP2optFirst(wi).catch(handleError);
-    console.log(`2-opt First: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('2-opt Best', async () => {
-    await TSP2optBest(wi).catch(handleError);
-    console.log(`2-opt Best: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('Ant System', async () => {
-    await TSPAntSystem(wi).catch(handleError);
-    console.log(`Ant System: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('Lin-Kernighan', async () => {
-    await TSPLinKernighan(wi).catch(handleError);
-    console.log(`Lin-Kernighan: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('Nearest Neighbor', async () => {
-    await TSPNearestNeighbor(wi).catch(handleError);
-    console.log(`Nearest Neighbor: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('Custom', async () => {
-    const code = fs.readFileSync(require.resolve('../src/custom/nearestNeighbor'), 'utf8');
-    wi.params = { code };
-    await TSPCustom(wi).catch(handleError);
-    console.log(`Custom: ${Math.floor(wi.cost)} km`);
-  });
-});
-
-describe('Spain, size 1135', () => {
-  let wi: WorkerInstance;
-  let markers = prepareMarkers(Spain);
-
-  beforeEach(() => {
-    global.console = require('console');
-    wi = new WorkerInstance();
-    wi.performanceMode = true;
-    wi.markers = [...markers];
-  });
-
-  afterEach(() => {
-    global.console = jestConsole;
-  });
-
-  it('2-opt First', async () => {
-    console.log('\nRunning tests for Spain (size 1135)...\n');
-    await TSP2optFirst(wi).catch(handleError);
-    console.log(`2-opt First: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('2-opt Best', async () => {
-    await TSP2optBest(wi).catch(handleError);
-    console.log(`2-opt Best: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('Ant System', async () => {
-    await TSPAntSystem(wi).catch(handleError);
-    console.log(`Ant System: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('Lin-Kernighan', async () => {
-    await TSPLinKernighan(wi).catch(handleError);
-    console.log(`Lin-Kernighan: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('Nearest Neighbor', async () => {
-    await TSPNearestNeighbor(wi).catch(handleError);
-    console.log(`Nearest Neighbor: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('Custom', async () => {
-    const code = fs.readFileSync(require.resolve('../src/custom/nearestNeighbor'), 'utf8');
-    wi.params = { code };
-    await TSPCustom(wi).catch(handleError);
-    console.log(`Custom: ${Math.floor(wi.cost)} km`);
-  });
-});
-
-describe('France, size 2019', () => {
-  let wi: WorkerInstance;
-  let markers = prepareMarkers(France);
-
-  beforeEach(() => {
-    global.console = require('console');
-    wi = new WorkerInstance();
-    wi.performanceMode = true;
-    wi.markers = [...markers];
-  });
-
-  afterEach(() => {
-    global.console = jestConsole;
-  });
-
-  it('2-opt First', async () => {
-    console.log('\nRunning tests for France (size 2019)...\n');
-    await TSP2optFirst(wi).catch(handleError);
-    console.log(`2-opt First: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('2-opt Best', async () => {
-    await TSP2optBest(wi).catch(handleError);
-    console.log(`2-opt Best: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('Ant System', async () => {
-    await TSPAntSystem(wi).catch(handleError);
-    console.log(`Ant System: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('Lin-Kernighan', async () => {
-    await TSPLinKernighan(wi).catch(handleError);
-    console.log(`Lin-Kernighan: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('Nearest Neighbor', async () => {
-    await TSPNearestNeighbor(wi).catch(handleError);
-    console.log(`Nearest Neighbor: ${Math.floor(wi.cost)} km`);
-  });
-
-  it('Custom', async () => {
-    const code = fs.readFileSync(require.resolve('../src/custom/nearestNeighbor'), 'utf8');
-    wi.params = { code };
-    await TSPCustom(wi).catch(handleError);
-    console.log(`Custom: ${Math.floor(wi.cost)} km`);
+    it('Custom', async () => {
+      const code = fs.readFileSync(require.resolve('../src/custom/nearestNeighbor'), 'utf8');
+      wi.params = { code };
+      await TSPCustom(wi).catch(handleError);
+      console.log(`Custom: ${Math.floor(wi.cost)} km`);
+    });
   });
 });
