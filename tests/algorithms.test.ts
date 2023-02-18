c63c5022b56921813c13c4f69700747e04218e4e
Dawid Rodak <dawidrodak@yahoo.com>
2023-02-18 19:59:50 +0100

    Update algorithms tests

--- tests/algorithms.test.ts
+++ tests/algorithms.test.ts
@@ -1,194 +1,101 @@
-import fs from 'fs';
-
-import { describe } from '@jest/globals';
+import { test } from '@jest/globals';
 import { TSP2optFirst } from '../src/workers/Tsp2optFirst';
 import { WorkerInstance } from '../src/workers/createWorker';
 import { TSP2optBest } from '../src/workers/Tsp2optBest';
 import { TSPAntSystem } from '../src/workers/TSPAntSystem';
-import { TSPCustom } from '../src/workers/TSPCustom';
 import { TSPLinKernighan } from '../src/workers/TSPLinKernighan';
 import { TSPNearestNeighbor } from '../src/workers/TSPNearestNeighbor';
-import { France } from '../src/datasets/France';
 import { Spain } from '../src/datasets/Spain';
 import { Canada } from '../src/datasets/Canada';
 import { Poland } from '../src/datasets/Poland';
 import { prepareMarkers } from '../src/helpers';
-
-const handleError = (err: any) => {
-  if (err === 'Stopped') {
-    return;
-  }
-  throw err;
-};
+import _ from 'lodash';
+import { Marker, WorkerInterface } from '../src/types';
 
 const jestConsole = console;
 
-describe('Algorithms', () => {
-  let wi: WorkerInstance;
+const TEST_TIMES = 30;
 
-  beforeEach(() => {
-    global.console = require('console');
-    wi = new WorkerInstance();
-    wi.performanceMode = true;
-  });
+const ALGORITHMS = [
+  { name: 'Ant System', algorithm: TSPAntSystem },
+  { name: 'Lin-Kernighan', algorithm: TSPLinKernighan },
+  { name: '2-opt Best', algorithm: TSP2optBest },
+  { name: '2-opt First', algorithm: TSP2optFirst },
+  { name: 'Nearest Neighbor', algorithm: TSPNearestNeighbor },
+];
 
-  afterEach(() => {
-    global.console = jestConsole;
-  });
+const DATASETS = [
+  { name: 'Poland', markers: Poland },
+  { name: 'Canada', markers: Canada },
+  { name: 'Spain', markers: Spain },
+];
 
-  describe('Poland, size 291', () => {
-    beforeEach(() => {
-      wi.markers = prepareMarkers(Poland);
-    });
-
-    it('2-opt First', async () => {
-      console.log('\nRunning tests for Poland (size 291)...\n');
-      await TSP2optFirst(wi).catch(handleError);
-      console.log(`2-opt First: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('2-opt Best', async () => {
-      await TSP2optBest(wi).catch(handleError);
-      console.log(`2-opt Best: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Ant System', async () => {
-      await TSPAntSystem(wi).catch(handleError);
-      console.log(`Ant System: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Lin-Kernighan', async () => {
-      await TSPLinKernighan(wi).catch(handleError);
-      console.log(`Lin-Kernighan: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Nearest Neighbor', async () => {
-      await TSPNearestNeighbor(wi).catch(handleError);
-      console.log(`Nearest Neighbor: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Custom', async () => {
-      const code = fs.readFileSync(require.resolve('../src/custom/nearestNeighbor'), 'utf8');
-      wi.params = { code };
-      await TSPCustom(wi).catch(handleError);
-      console.log(`Custom: ${Math.floor(wi.cost)} km`);
-    });
-  });
-
-  describe('Canada, size 625', () => {
-    beforeEach(() => {
-      wi.markers = prepareMarkers(Canada);
-    });
-
-    it('2-opt First', async () => {
-      console.log('\nRunning tests for Canada (size 625)...\n');
-      await TSP2optFirst(wi).catch(handleError);
-      console.log(`2-opt First: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('2-opt Best', async () => {
-      await TSP2optBest(wi).catch(handleError);
-      console.log(`2-opt Best: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Ant System', async () => {
-      await TSPAntSystem(wi).catch(handleError);
-      console.log(`Ant System: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Lin-Kernighan', async () => {
-      await TSPLinKernighan(wi).catch(handleError);
-      console.log(`Lin-Kernighan: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Nearest Neighbor', async () => {
-      await TSPNearestNeighbor(wi).catch(handleError);
-      console.log(`Nearest Neighbor: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Custom', async () => {
-      const code = fs.readFileSync(require.resolve('../src/custom/nearestNeighbor'), 'utf8');
-      wi.params = { code };
-      await TSPCustom(wi).catch(handleError);
-      console.log(`Custom: ${Math.floor(wi.cost)} km`);
-    });
-  });
-
-  describe('Spain, size 1135', () => {
-    beforeEach(() => {
-      wi.markers = prepareMarkers(Spain);
-    });
-
-    it('2-opt First', async () => {
-      console.log('\nRunning tests for Spain (size 1135)...\n');
-      await TSP2optFirst(wi).catch(handleError);
-      console.log(`2-opt First: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('2-opt Best', async () => {
-      await TSP2optBest(wi).catch(handleError);
-      console.log(`2-opt Best: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Ant System', async () => {
-      await TSPAntSystem(wi).catch(handleError);
-      console.log(`Ant System: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Lin-Kernighan', async () => {
-      await TSPLinKernighan(wi).catch(handleError);
-      console.log(`Lin-Kernighan: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Nearest Neighbor', async () => {
-      await TSPNearestNeighbor(wi).catch(handleError);
-      console.log(`Nearest Neighbor: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Custom', async () => {
-      const code = fs.readFileSync(require.resolve('../src/custom/nearestNeighbor'), 'utf8');
-      wi.params = { code };
-      await TSPCustom(wi).catch(handleError);
-      console.log(`Custom: ${Math.floor(wi.cost)} km`);
-    });
-  });
-
-  describe('France, size 2019', () => {
-    beforeEach(() => {
-      wi.markers = prepareMarkers(France);
-    });
-
-    it('2-opt First', async () => {
-      console.log('\nRunning tests for France (size 2019)...\n');
-      await TSP2optFirst(wi).catch(handleError);
-      console.log(`2-opt First: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('2-opt Best', async () => {
-      await TSP2optBest(wi).catch(handleError);
-      console.log(`2-opt Best: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Ant System', async () => {
-      await TSPAntSystem(wi).catch(handleError);
-      console.log(`Ant System: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Lin-Kernighan', async () => {
-      await TSPLinKernighan(wi).catch(handleError);
-      console.log(`Lin-Kernighan: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Nearest Neighbor', async () => {
-      await TSPNearestNeighbor(wi).catch(handleError);
-      console.log(`Nearest Neighbor: ${Math.floor(wi.cost)} km`);
-    });
-
-    it('Custom', async () => {
-      const code = fs.readFileSync(require.resolve('../src/custom/nearestNeighbor'), 'utf8');
-      wi.params = { code };
-      await TSPCustom(wi).catch(handleError);
-      console.log(`Custom: ${Math.floor(wi.cost)} km`);
-    });
-  });
+test('Algorithms', async () => {
+  global.console = require('console');
+  for (const { name: datasetName, markers } of DATASETS) {
+    for (const { name: algorithmName, algorithm } of ALGORITHMS) {
+      console.log(`\n${datasetName} (${markers.length}) - ${algorithmName}\n`);
+      await testAlgorithm(algorithm, markers);
+    }
+  }
+  global.console = jestConsole;
 });
+
+async function testAlgorithm(
+  algorithm: (app: Readonly<WorkerInterface>) => Promise<void>,
+  markers: Marker[],
+  params = {}
+) {
+  const results: { cost: number; time: number }[] = [];
+
+  for (const idx of new Array(TEST_TIMES).fill(0).keys()) {
+    const wi = new WorkerInstance();
+    wi.markers = prepareMarkers(markers);
+    wi.performanceMode = true;
+    wi.params = params;
+
+    const start = Date.now();
+    await algorithm(wi).catch((err) => {
+      if (err !== 'Stopped') {
+        throw err;
+      }
+    });
+    const time = (Date.now() - start) / 1000;
+    const cost = wi.cost;
+
+    console.log(`${idx + 1}. Cost: ${fmt(cost)} km, Time: ${time} s`);
+    results.push({ cost, time });
+  }
+
+  const minTime = _.minBy(results, 'time')!.time;
+  const maxTime = _.maxBy(results, 'time')!.time;
+  const avgTime = results.reduce((acc, { time }) => acc + time, 0) / results.length;
+  const stdDevTime = Math.sqrt(
+    results.reduce((acc, { time }) => acc + Math.pow(time - avgTime, 2), 0) / results.length
+  );
+
+  const minCost = _.minBy(results, 'cost')!.cost;
+  const maxCost = _.maxBy(results, 'cost')!.cost;
+  const avgCost = results.reduce((acc, { cost }) => acc + cost, 0) / results.length;
+  const stdDevCost = Math.sqrt(
+    results.reduce((acc, { cost }) => acc + Math.pow(cost - avgCost, 2), 0) / results.length
+  );
+
+  const table = {
+    minTime: fmt(minTime),
+    maxTime: fmt(maxTime),
+    avgTime: fmt(avgTime),
+    stdDevTime: fmt(stdDevTime),
+    minCost: fmt(minCost),
+    maxCost: fmt(maxCost),
+    avgCost: fmt(avgCost),
+    stdDevCost: fmt(stdDevCost),
+  };
+
+  console.log();
+  console.table(table);
+}
+
+function fmt(str: number) {
+  return Number(str.toFixed(2));
+}
